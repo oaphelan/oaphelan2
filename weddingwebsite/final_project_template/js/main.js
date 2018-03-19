4db692f06c26a5f824a28f0596db0bf3b450828f
oaphelan <35713052+oaphelan@users.noreply.github.com>
2018-03-19 14:26:24 -0400

    Add files via upload

--- weddingwebsite/final_project_template/js/main.js
+++ weddingwebsite/final_project_template/js/main.js
@@ -1,1 +1,16 @@
-// Make it rain!
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	var distanceScrolled = $(window).scrollTop();
+
+
+		if(distanceScrolled > 70) {
+			$('.celebration').fadeIn('slow');
+			$('.story').fadeIn('slow');
+		}
+		 	else {
+			$('.celebration').fadeOut('slow');
+			$('.story').fadeOut('slow');
+		}
+
+console.log(distanceScrolled);
+});
